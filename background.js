c1ab8a86f780f335a2c439631c440f90d91b5f03
Giuseppe Attanasio <master94ga@gmail.com>
2014-11-30 11:35:53 +0100

    Second build
    Add var to background and created settings file.

--- background.js
+++ background.js
@@ -1,5 +1,7 @@
+var nickname = "master94ga";
+
 chrome.browserAction.onClicked.addListener(function() {
   chrome.tabs.query({currentWindow: true, active: true}, function(tab) {
-    chrome.tabs.create( { "url": "http://op.gg" } );
+    chrome.tabs.create( { "url": "http://euw.op.gg/summoner/userName=" +nickname } );
   });
 });
