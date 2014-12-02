d1f527263d8fb981a8e484723a1ff757983ebb2e
Giuseppe Attanasio <master94ga@gmail.com>
2014-12-02 11:20:55 +0100

    First version of javascript part of settings

---
+++ settings.js
@@ -0,0 +1,16 @@
+//Save the nickname
+function Save(){
+	var input = document.getElementById("value1");
+    localStorage.setItem("key1", input.value);
+}
+
+//Load the nickname from local storage
+function Load(){
+    var key1 = localStorage.getItem("key1"); //Load the nickname from the local storage
+    document.getElementById("nicknameonpage").innerHTML = localStorage.getItem("key1");
+}
+
+//Restore to default
+function Default(){
+localStorage.removeItem("key1");
+}
