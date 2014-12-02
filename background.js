3a9db34f495dcd52190e33ec7cd50d15626887c5
Giuseppe Attanasio <master94ga@gmail.com>
2014-12-02 17:43:57 +0100

    Added new var, need load from local storage

--- background.js
+++ background.js
@@ -1,7 +1,8 @@
-var nickname = localStorage.getItem("key1");
+var link1 = items.yourserver;
+var link2 = items.yournickname;
 
 chrome.browserAction.onClicked.addListener(function() {
   chrome.tabs.query({currentWindow: true, active: true}, function(tab) {
-    chrome.tabs.create( { "url": "http://euw.op.gg/summoner/userName=" +nickname } );
+    chrome.tabs.create( { "url": "http://"+link1+".op.gg/summoner/userName="+link2 } );
   });
 });
