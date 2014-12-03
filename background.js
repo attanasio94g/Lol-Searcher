6a257da02b45079dbad29156f9c835d34ab94f53
Giuseppe Attanasio <master94ga@gmail.com>
2014-12-03 20:48:19 +0100

    Load from storage on startup

--- background.js
+++ background.js
@@ -1,15 +1,25 @@
 var link1;
 var link2;
 
-function loadvalue(){
- chrome.storage.sync.get({
+chrome.runtime.onStartup.addListener(function(){
+   chrome.storage.sync.get({
     yourserver: 'kr',
     yournickname: null
   }, function(items) {
     link1 = items.yourserver;
     link2 = items.yournickname;
   });
-}
+});
+
+chrome.browserAction.onClicked.addListener(function(){
+   chrome.storage.sync.get({
+    yourserver: 'kr',
+    yournickname: null
+  }, function(items) {
+    link1 = items.yourserver;
+    link2 = items.yournickname;
+  });
+});
 
 chrome.browserAction.onClicked.addListener(function() {
   chrome.tabs.query({currentWindow: true, active: true}, function(tab) {
