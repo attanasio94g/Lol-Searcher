4d0ec0093dfeb6f790e5a2565620d1cf40612300
Giuseppe Attanasio <master94ga@gmail.com>
2014-12-25 17:21:32 +0100

    Ready for release

---
+++ build/0.1/options.js
@@ -0,0 +1,36 @@
+// Saves options to chrome.storage
+function save_options() {
+  var server = document.getElementById('server').value;
+  var nickname = document.getElementById('nickname').value;
+  chrome.storage.sync.set({
+    yourserver: server,
+    yournickname: nickname
+  }, function() {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+  });
+}
+
+function reload_page()
+{
+  location.reload();
+}
+
+// Restores select box and text using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  // Use default value server = 'TR' and nickname = null.
+  chrome.storage.sync.get({
+    yourserver: null,
+    yournickname: null
+  }, function(items) {
+    document.getElementById('server').value = items.yourserver;
+    document.getElementById("nicknameonpage").innerHTML = items.yournickname;
+  });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click', function() 
+    {
+    save_options()
+    reload_page()
+    });
